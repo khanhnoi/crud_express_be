90267698b37e5aff6e9f5bdd7c5f7256adca6d2d
khanhnoi <nqkhanh1998@gmail.com>
2021-02-25 02:07:48 +0700

    crud done

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,11 @@
+const mongoose = require("mongoose");
 const PostMessage = require("../models/postMessage");
 
 const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
-    console.log("postMessages");
-    console.log(postMessages);
+    // console.log("postMessages");
+    // console.log(postMessages);
 
     res.status(200).json(postMessages);
   } catch (error) {
@@ -28,7 +29,63 @@ const createPost = async (req, res) => {
   }
 };
 
+const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const post = req.body;
+  // const newPost = new PostMessage(post);
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(404).send("No post with that Id !!");
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+      new: true,
+    });
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(404).send("No post with that Id");
+
+    const updatedPost = await PostMessage.findByIdAndRemove(id);
+    res.status(200).json("Delete Post Success !!");
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+const likePost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      return res.status(404).send("No post with that Id");
+
+    const post = await PostMessage.findById(id);
+    const numberLikeCurrent = post.likeCount;
+    const updatePost = await PostMessage.findByIdAndUpdate(
+      id,
+      {
+        likeCount: numberLikeCurrent + 1,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatePost);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getPosts,
   createPost,
+  updatePost,
+  deletePost,
+  likePost,
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,18 @@
 const expess = require("express");
 const router = expess.Router();
 
-const { getPosts, createPost } = require("../controllers/posts");
+const {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+} = require("../controllers/posts");
 
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.patch("/:id/like", likePost);
 
 module.exports = router;
